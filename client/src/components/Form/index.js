3cfa7186a77e6263cd936b389b62fda15cf0dde4
jamie <jamiefhill@gmail.com>
2020-05-13 14:09:36 +0100

    Removed console.log

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 function Form({ children }) {
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		console.log(event.target);
 	};
 
 	return <form onSubmit={handleSubmit}>{children}</form>;
