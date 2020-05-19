720589f37ebf271d861b52b0792d1b93a2438071
jamie <jamiefhill@gmail.com>
2020-05-19 17:52:57 +0100

    Updated post end point to insert new contact messages to the database

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const upload = multer();
 const PORT = process.env.PORT || 8080;
 const app = express();
+const httpStatusCodes = require('http-status-codes');
 
 // Deal with CORS
 app.use(cors());
@@ -23,6 +24,64 @@ db.connect((err) => {
 	if (err) throw err;
 	console.log('MySQL Connected...');
 });
+
+//QUERY
+const query = (sql, args) => {
+	return new Promise((resolve, reject) => {
+		db.query(sql, args, (err, rows) => {
+			if (err) return reject(err);
+			resolve(rows);
+		});
+	});
+};
+
+const findAuthorId = async (email) => {
+	const author = await query(`select id from contact where email = ?`, email);
+	if (author[0]) {
+		return author[0].id;
+	}
+};
+
+const addAuthor = async (
+	firstname,
+	lastname,
+	email,
+	telephone,
+	housenumber,
+	street,
+	city,
+	county,
+	country
+) => {
+	const result = await query(
+		`insert into contact (first_name, last_name, email, telephone, house_number, street, city, county, country) values (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+		[
+			firstname,
+			lastname,
+			email,
+			telephone,
+			housenumber,
+			street,
+			city,
+			county,
+			country,
+		]
+	);
+	if (result.affectedRows === 1) {
+		return result.insertId;
+	}
+};
+
+const addMessage = async (authorId, message) => {
+	const result = await query(
+		`insert into messages (author, message) values (?, ?)`,
+		[authorId, message]
+	);
+	if (result.affectedRows === 1) {
+		console.log('New contact message added to the database!');
+		return result.insertId;
+	}
+};
 
 // Start Express listening
 app.listen(PORT, () => {
@@ -54,12 +113,30 @@ app.post('/', (req, res) => {
 });
 
 // An API endpoint to store form post data
-app.post('/create', upload.none(), (req, res) => {
-	console.log('Body', req.body);
-	return res.json({
-		status: res.statusCode,
-		data: {
-			message: req.body.city,
-		},
-	});
+app.post('/create', upload.none(), async (req, res) => {
+	let authorId = await findAuthorId(req.body.email);
+	if (!authorId) {
+		authorId = await addAuthor(
+			req.body.firstname,
+			req.body.lastname,
+			req.body.email,
+			req.body.telephone,
+			req.body.housenumber,
+			req.body.street,
+			req.body.city,
+			req.body.county,
+			req.body.country
+		);
+		if (!authorId) {
+			return res.sendStatus(httpStatusCodes.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	const messageId = await addMessage(authorId, req.body.message);
+
+	if (messageId) {
+		res.sendStatus(httpStatusCodes.OK);
+	} else {
+		res.sendStatus(httpStatusCodes.INTERNAL_SERVER_ERROR);
+	}
 });
