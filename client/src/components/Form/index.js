d7a07cb7ca6ee46f04320c987e137d18078dc486
jamie <jamiefhill@gmail.com>
2020-05-19 15:03:08 +0100

    Fixed linting issue in form submission

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import superagent from 'superagent';
 
 function Form({ children }) {
-	const handleSubmit = (event) => {
+	const handleSubmit = async (event) => {
 		event.preventDefault();
+		const data = new FormData(event.target);
+		await superagent.post('http://localhost:8080/create').send(data);
 	};
 
 	return <form onSubmit={handleSubmit}>{children}</form>;
