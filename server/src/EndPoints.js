e5e01de7c23c41c862dc6a347abf149f25c71e23
jamie <jamiefhill@gmail.com>
2020-05-21 18:45:13 +0100

    Added swagger api documentation generation

--- server/src/EndPoints.js
+++ server/src/EndPoints.js
@@ -8,6 +8,20 @@ const {
 	TEST_END_POINT_HIT,
 } = require('./CopyContants');
 
+/**
+ * @swagger
+ *
+ * /:
+ *   get:
+ *     description: Test end point, alerts the front end the service is running
+ *     produces:
+ *       - text/plain; charset=utf-8
+ *     consumes:
+ *       - text/plain; charset=utf-8
+ *     responses:
+ *       200:
+ *         description: Active api message is returned
+ */
 const testController = (req, res) => {
 	console.log(TEST_END_POINT_HIT);
 	return res.json({
@@ -18,6 +32,20 @@ const testController = (req, res) => {
 	});
 };
 
+/**
+ * @swagger
+ *
+ * /:
+ *   post:
+ *     description: Echo a message for unknown reasons
+ *     produces:
+ *       - text/plain; charset=utf-8
+ *     consumes:
+ *       - text/plain; charset=utf-8
+ *     responses:
+ *       200:
+ *         description: Body is returned
+ */
 const echoController = (req, res) => {
 	return res.json({
 		status: res.statusCode,
@@ -28,6 +56,71 @@ const echoController = (req, res) => {
 	});
 };
 
+/**
+ * @swagger
+ *
+ * /create:
+ *   post:
+ *     description: Allows a user to send a contact request
+ *     produces:
+ *       - text/plain; charset=utf-8
+ *     consumes:
+ *       - multipart/form-data
+ *     parameters:
+ *       - name: firstname
+ *         description: The firstname of the user
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: lastname
+ *         description: The lastname of the user
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: email
+ *         description: The users email address
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: telephone
+ *         description: The users telephone number
+ *         in: formData
+ *         required: true
+ *         type: number
+ *       - name: housenumber
+ *         description: The users house number
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: street
+ *         description: The users street
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: city
+ *         description: The users city
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: county
+ *         description: The users county
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: country
+ *         description: The users country
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: message
+ *         description: The message associated with the contact request
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Contact request was successfully submitted
+ */
 const createController = async (req, res) => {
 	validateContactForm(req.body);
 	const authorId = await findOrAddAuthor(
---
+++ server/swagger/swaggerDef.js
@@ -0,0 +1,10 @@
+module.exports = {
+	info: {
+		title: 'Contact Request API',
+		version: '1.0.0',
+		description: 'Contact Request API',
+	},
+	host: 'localhost:3001',
+	apis: ['src/**/*.js'],
+	basePath: '/',
+};
